99fa566a34b69ef44650d98bd8a9cea8f6335b1a
inshining <inshining.kim@gmail.com>
2022-12-17 18:08:36 +0900

    😺[CHORE] : db 연결 테스트

---
+++ boilerplate/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+
+type Item struct {
+	gorm.Model
+	Title string
+	Category string
+	Price int 
+	Content string
+}
+
+func main() {
+	dsn := "test:asdf0102!@tcp(localhost:3306)/mydb?parseTime=true"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	db.AutoMigrate(&Item{})
+
+	if err != nil{
+		panic("DB 연결이 실패했습니다!")
+	}
+
+	fmt.Println("연결이 성공적으로 ")
+}
