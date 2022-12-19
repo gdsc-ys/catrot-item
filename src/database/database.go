ebdc9d06b361596b93e416e6e76bf675c26ce86e
inshining <inshining.kim@gmail.com>
2022-12-19 19:09:55 +0900

    😺[CHORE] : db session 발급

--- src/database/database.go
+++ src/database/database.go
@@ -6,22 +6,24 @@ import (
 	"gorm.io/gorm"
 )
 
-
 type Item struct {
 	gorm.Model
-	Title string
+	Title    string
 	Category string
-	Price int 
-	Content string
+	Price    int
+	Content  string
 }
 
+var DB *gorm.DB
+
 func Init() {
+	var err error
 	dsn := "test:asdf0102!@tcp(localhost:3306)/mydb?parseTime=true"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	db.AutoMigrate(&Item{})
+	DB.AutoMigrate(&Item{})
 
-	if err != nil{
+	if err != nil {
 		panic("DB 연결이 실패했습니다!")
 	}
 
