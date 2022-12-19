fe0af6dee196261e8f3b4fdedadeb0da53402404
inshining <inshining.kim@gmail.com>
2022-12-19 19:10:36 +0900

    😺[CHORE] : items list handler

--- src/handlers/handlers.go
+++ src/handlers/handlers.go
@@ -1,37 +1,46 @@
 package handlers
 
 import (
-	"catrot-item/boilerplate/database"
-	"catrot-item/boilerplate/models"
-
+	"src/database"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/utils"
+	// "github.com/gofiber/fiber/v2/utils"
 )
 
-// UserGet returns a user
-func UserList(c *fiber.Ctx) error {
-	users := database.Get()
-
+func ItemList(c *fiber.Ctx) error {
+	// var items []database.Item
+	var item database.Item
+	db := database.DB
+	db.First(&item)
 	return c.JSON(fiber.Map{
-		"success": true,
-		"users":   users,
+		"success":true,
+		"itmes": item,
 	})
 }
 
-// UserCreate registers a user
-func UserCreate(c *fiber.Ctx) error {
-	user := &models.User{
-		// Note: when writing to external database,
-		// we can simply use - Name: c.FormValue("user")
-		Name: utils.CopyString(c.FormValue("user")),
-	}
-	database.Insert(user)
+// // UserGet returns a user
+// func UserList(c *fiber.Ctx) error {
+// 	users := database.Get()
 
-	return c.JSON(fiber.Map{
-		"success": true,
-		"user":    user,
-	})
-}
+// 	return c.JSON(fiber.Map{
+// 		"success": true,
+// 		"users":   users,
+// 	})
+// }
+
+// // UserCreate registers a user
+// func UserCreate(c *fiber.Ctx) error {
+// 	user := &models.User{
+// 		// Note: when writing to external database,
+// 		// we can simply use - Name: c.FormValue("user")
+// 		Name: utils.CopyString(c.FormValue("user")),
+// 	}
+// 	database.Insert(user)
+
+// 	return c.JSON(fiber.Map{
+// 		"success": true,
+// 		"user":    user,
+// 	})
+// }
 
 // NotFound returns custom 404 page
 func NotFound(c *fiber.Ctx) error {
