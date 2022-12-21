b5204c64caf0ab6e523734a56c9616769344e802
inshining <inshining.kim@gmail.com>
2022-12-21 22:58:22 +0900

    🗑[DEL] : 안쓰는 유저 코드 삭제

--- src/handlers/handlers.go
+++ src/handlers/handlers.go
@@ -5,7 +5,6 @@ import (
 	"src/models"
 
 	"github.com/gofiber/fiber/v2"
-	// "github.com/gofiber/fiber/v2/utils"
 )
 
 func ItemList(c *fiber.Ctx) error {
@@ -28,31 +27,6 @@ func ItemDetail(c *fiber.Ctx) error {
 		"item": item,
 	})
 }
-
-// // UserGet returns a user
-// func UserList(c *fiber.Ctx) error {
-// 	users := database.Get()
-
-// 	return c.JSON(fiber.Map{
-// 		"success": true,
-// 		"users":   users,
-// 	})
-// }
-
-// // UserCreate registers a user
-// func UserCreate(c *fiber.Ctx) error {
-// 	user := &models.User{
-// 		// Note: when writing to external database,
-// 		// we can simply use - Name: c.FormValue("user")
-// 		Name: utils.CopyString(c.FormValue("user")),
-// 	}
-// 	database.Insert(user)
-
-// 	return c.JSON(fiber.Map{
-// 		"success": true,
-// 		"user":    user,
-// 	})
-// }
 
 // NotFound returns custom 404 page
 func NotFound(c *fiber.Ctx) error {
