abb5ce6d979b6b1a29e4724f4a3012cc91ec3353
inshining <inshining.kim@gmail.com>
2023-01-01 23:25:17 +0900

    😺[CHORE] : .env 파일 설정

--- src/database/database.go
+++ src/database/database.go
@@ -1,18 +1,32 @@
 package database
 
 import (
-	"fmt"
+	"log"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"src/models"
+	"os"
+	"github.com/joho/godotenv"
 )
 
 
 var DB *gorm.DB
 
 func Init() {
+	dotEnvErr := godotenv.Load()
+	if dotEnvErr != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?parseTime=true"
+
 	var err error
-	dsn := "test:asdf0102!@tcp(localhost:3306)/mydb?parseTime=true"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	DB.AutoMigrate(&models.Item{})
@@ -21,5 +35,5 @@ func Init() {
 		panic("DB 연결이 실패했습니다!")
 	}
 
-	fmt.Println("연결이 성공적으로 ")
+	log.Println("연결이 성공적으로 ")
 }
